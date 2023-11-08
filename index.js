e4a9d8b2fbc7b3755289222874bf431d492924d3
Mahmud Munna <smbmunna@gmail.com>
2023-11-08 08:27:47 +0600

    server issue fixed

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const cart = database.collection("cart");
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
